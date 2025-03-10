bb9f161131bab3bc384a908160d8ed39fdee1999
andermaco@gmail.com <andermaco@gmail.com>
2025-03-10 14:21:26 +0100

    printout

--- nyc_parking_violations_B/tests/generic/generic_not_null.sql
+++
@@ -1,8 +0,0 @@
--- source: https://docs.getdbt.com/guides/best-practices/writing-custom-generic-tests
-{% test generic_not_null(model, column_name) %}
-
-SELECT *
-FROM {{ model }}
-WHERE {{ column_name }} IS NULL
-
-{% endtest %}
